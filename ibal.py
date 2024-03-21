bb11a8bddc5ae4e3fe88cca6d3042d9f182fd138
mhmdiqbal_23 <163904070+mhmmmdibal@users.noreply.github.com>
2024-03-22 00:52:43 +0700

    Update ibal.py

--- ibal.py
+++ ibal.py
@@ -85,9 +85,9 @@ class Logo:
                               {M2}██████████████████████[/]
                                {P2}██████████████████████[/]
                                {P2}██████████████████████[/]
-                                     Made By {M2}Indonesia {P2}Coder
+                                     Made By {U2}Indonesia {U2}Coder
 {U2}╭──────────────────────╮{U2}╭───────────────╮{U2}╭────────────────────────────╮
-{U2}│ {U2}Author : mhmmdibal {U2}│{U2}│ {U2}Version : 3.1 {U2}│{U2}│ {U2}Recode By : IQBAL_Xyz {B2}       │
+{U2}│ {U2}Author : mhmmdibal {U2}│{U2}│ {U2}Version : 3.1 {U2}│{U2}│ {U2}Recode By : IQBAL_Xyz {U2}       │
 {U2}╰──────────────────────╯{U2}╰───────────────╯{U2}╰────────────────────────────╯""",width=80,style=f"{color_panel}"))
 	
 	###----------[ BAGIAN LOGIN ]---------- ###
