a5d4237849823740459f131ed44362178fa0e18a
mhmdiqbal_23 <163904070+mhmmmdibal@users.noreply.github.com>
2024-03-22 03:14:12 +0700

    Update ibal.py

--- ibal.py
+++ ibal.py
@@ -346,7 +346,7 @@ class Crack:
 						fall.submit(self.metode_api,user,pwx)
 					except:
 						fall.submit(self.metode_api,user,pwx)
-		prints(Panel(f"""{U2}berhasil crack total {len(tampung)} id, dengan hasil OK : {H2}{len(self.ok)}{M2} CP : {U2}{len(self.cp)}{M2}""",width=80,padding=(0,8),style=f"{color_panel}"))
+		prints(Panel(f"""{U2}berhasil crack total {len(tampung)} id, dengan hasil OK : {H2}{len(self.ok)}{M2} CP : {M2}{len(self.cp)}{U2}""",width=80,padding=(0,8),style=f"{color_panel}"))
 		sys.exit()
 							
 	###----------[ METODE API ]---------- ###
