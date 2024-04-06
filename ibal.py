537e5618e6c854adb665e04807c34855279de0e7
mhmdiqbal_23 <163904070+mhmmmdibal@users.noreply.github.com>
2024-04-06 20:00:47 +0700

    Update ibal.py

--- ibal.py
+++ ibal.py
@@ -58,7 +58,7 @@ for z in range(200):
 	versi = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_3"])
 	verchrome = random.choice(["602.1.50","602.2.14","602.3.12","602.4.6","603.1.30","603.2.4","603.3.8","601.1.46"])
 	mob = random.choice(["14A456","14B100","14C92","14D27","14E304","14F89","14G60","13C75","13D15","13E233","13E238","13F69","13G34","13G36"])
-	ua = f"Mozilla/5.0 (Linux; Android {android}; HTC U23 Build/1.14.709.2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{versi_chrome} Mobile Safari/537.36 [FBAN/EMA;FBLC/de_DE;FBAV/{str(rr(1000,4000))}.0.0.{str(rr(1,400))}.{str(rr(1000,450000))};]"
+	ua = f"Mozilla/5.0 (Linux; Android {android}; HTC U23 Build/{build}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{versi_chrome} Mobile Safari/537.36 [FBAN/EMA;FBLC/de_DE;FBAV/{str(rr(1000,4000))}.0.0.{str(rr(1,400))}.{str(rr(1000,450000))};]"
 	if ua in ugent:pass
 	else:ugent.append(ua)
 	
